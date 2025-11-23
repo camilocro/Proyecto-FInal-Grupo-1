39443312dd322ffeaeb95cc8a69bb0f78675349a
isa2206 <hidalgoavilaisabel@gmail.com>
2025-11-23 11:47:50 -0400

    Feat: Sponsor.cs

--- Models/Sponsor.cs
+++ Models/Sponsor.cs
@@ -1,6 +1,13 @@
-﻿namespace Proyecto_FInal_Grupo_1.Models
+﻿
+namespace Proyecto_FInal_Grupo_1.Models
 {
     public class Sponsor
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Industry { get; set; }
+        public decimal Amount { get; set; }
+        public ICollection<CarSponsor>? CarSponsors { get; set; }
+        public ICollection<Driver>? Drivers { get; set; }
     }
 }
