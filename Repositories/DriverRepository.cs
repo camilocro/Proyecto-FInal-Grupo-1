d8edfdedfc0d66b4035105104765146879d8e1d3
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-11-23 15:40:31 -0400

    feat(Repositories): Adding Driver Implementation

--- Repositories/DriverRepository.cs
+++ Repositories/DriverRepository.cs
@@ -1,6 +1,50 @@
-﻿namespace Proyecto_FInal_Grupo_1.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_FInal_Grupo_1.Data;
+using Proyecto_FInal_Grupo_1.Models;
+
+namespace Proyecto_FInal_Grupo_1.Repositories
 {
-    public class DriverRepository
+    public class DriverRepository : IDriverRepository
     {
+        private readonly AppDbContext _db;
+
+        public DriverRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task Add(Driver driver)
+        {
+            await _db.Drivers.AddAsync(driver);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Driver>> GetAll()
+        {
+            return await _db.Drivers
+                .Include(d => d.TeamCar)
+                .Include(d => d.Sponsor)
+                .ToListAsync();
+        }
+
+        public async Task<Driver?> GetOne(Guid id)
+        {
+            return await _db.Drivers
+                .Include(d => d.TeamCar)
+                .Include(d => d.Sponsor)
+                .FirstOrDefaultAsync(d => d.Id == id);
+        }
+
+        public async Task Update(Driver driver)
+        {
+            _db.Drivers.Update(driver);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Delete(Driver driver)
+        {
+            _db.Drivers.Remove(driver);
+            await _db.SaveChangesAsync();
+        }
     }
 }
